d7cc45ac1ef2a3c69c23bb933f19a080420f616a
chakrianekul1 <anekula@appsforbharat.com>
2026-01-20 10:43:38 +0530

    task3

---
+++ conc/square.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	nums := []int{2, 4, 6, 8, 10}
+	
+	// Create a channel to communicate the results
+	// We use a buffered channel with the size of the input slice
+	results := make(chan int, len(nums))
+
+	for _, n := range nums {
+		go func(val int) {
+			square := val * val
+			results <- square
+		}(n)
+	}
+
+	for i := 0; i < len(nums); i++ {
+		fmt.Printf("Result %d: %d\n", i+1, <-results)
+	}
+}
